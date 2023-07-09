29a7070ba846e80a9f55ca22870e0c1aebc4c2a1
Juhani Kauppi <jkauppi@student.hive.fi>
2023-07-09 19:45:34 +0300

    EX04 ready for test.

--- ex04/rotate.py
+++ ex04/rotate.py
@@ -10,6 +10,17 @@ def zoom_area() -> tuple:
     return (start_row, end_row, start_col, end_col)
 
 
+def rotate_90_and_flip(array: np.array) -> np.ndarray:
+    if len(array) > 0:
+        row:list = list(array[0])
+        rotated_image_array:np.ndarray = np.array(row)
+        for row in array[1:]:
+            rotated_image_array:np.ndarray = np.c_[rotated_image_array, row]
+    else:
+        rotated_image_array:np.ndarray = np.array([])
+    return rotated_image_array
+
+
 def main(path:str) -> None:
     '''
      A program which must load the image "animal.jpeg", cut a square part from it and transpose it to produce the image below.
@@ -19,14 +30,16 @@ def main(path:str) -> None:
     if len(image_array) > 0:
         (start_row, end_row, start_col, end_col) = zoom_area()
         zoomed_image_array:np.ndarray = image_array[start_row:end_row, start_col:end_col, 0]
-        zoomed_image:Image = Image.fromarray(zoomed_image_array)
         print('The shape of image is: {}'.format(zoomed_image_array.shape))
         print(zoomed_image_array)
-        transposed_image_array:Image = np.rot90(zoomed_image_array)
-        print('New shape after Transpose: {}'.format(transposed_image_array.shape))
-        print(transposed_image_array)
-        transposed_image:Image = Image.fromarray(transposed_image_array)
-        transposed_image.show()
+        rotated_image_array:np.ndarray = rotate_90_and_flip(zoomed_image_array)
+        print('New shape after Transpose: {}'.format(rotated_image_array.shape))
+        print(rotated_image_array)
+        try:
+            transposed_image:Image = Image.fromarray(rotated_image_array)
+            transposed_image.show()
+        except Exception as e:
+            print('{}: {}'.format(type(e).__name__, str(e)))
 
 if __name__ == '__main__':
     path:str = 'animal.jpeg'
