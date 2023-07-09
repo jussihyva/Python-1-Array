686845ddabc0d8d9bbd7c41adfd916023a313d67
Juhani Kauppi <juhani.kauppi@ericsson.com>
2023-07-10 00:35:29 +0300

    EX05 ready for test.

--- ex05/pimp_image.py
+++ ex05/pimp_image.py
@@ -1,6 +1,5 @@
 import numpy as np
 from PIL import Image
-import PIL.ImageOps
 
 def invert_pixcel(pixcel:list) -> list:
     inverted_pixcel:list = np.array(list(map(lambda x: np.uint8(255) - x, pixcel)))
@@ -11,62 +10,108 @@ def invert_row(row:list) -> list:
     return inverted_row
 
 
-def red_pixcel(pixcel:list) -> list:
-    red_color_pixcel:list = np.array([
-        np.uint8(255),
+def red_pixcel(pixcel:np.ndarray) -> np.ndarray:
+    red_color_pixcel:np.ndarray = np.array([
+        pixcel[0],
+        np.uint8(0),
+        np.uint8(0)
+    ])
+    return red_color_pixcel
+
+
+def green_pixcel(pixcel:np.ndarray) -> np.ndarray:
+    red_color_pixcel:np.ndarray = np.array([
+        np.uint8(0),
         pixcel[1],
+        np.uint8(0)
+    ])
+    return red_color_pixcel
+
+
+def blue_pixcel(pixcel:np.ndarray) -> np.ndarray:
+    red_color_pixcel:np.ndarray = np.array([
+        np.uint8(0),
+        np.uint8(0),
         pixcel[2]
     ])
     return red_color_pixcel
 
 
-def red_row(row:list) -> list:
-    red_row:np.ndarray = np.array(list(map(red_pixcel, row)))
-    # print('ROW: {}'.format(str(row)))
-    # print('RED_ROW: {}'.format(str(red_row)))
-    return red_row
+def grey_pixcel(pixcel:np.ndarray) -> np.ndarray:
+    value = np.mean(pixcel)
+    red_color_pixcel:np.ndarray = np.array([
+        np.uint8(value),
+        np.uint8(value),
+        np.uint8(value)
+    ])
+    return red_color_pixcel
+
+
+def red_row(row:np.ndarray) -> np.ndarray:
+    new_row:np.ndarray = np.array(list(map(red_pixcel, row)))
+    return new_row
+
+
+def green_row(row:np.ndarray) -> np.ndarray:
+    new_row:np.ndarray = np.array(list(map(green_pixcel, row)))
+    return new_row
+
+
+def blue_row(row:np.ndarray) -> np.ndarray:
+    new_row:np.ndarray = np.array(list(map(blue_pixcel, row)))
+    return new_row
+
+
+def grey_row(row:np.ndarray) -> np.ndarray:
+    new_row:np.ndarray = np.array(list(map(grey_pixcel, row)))
+    return new_row
 
 
 def ft_invert(array:np.ndarray) -> np.ndarray:
     '''
     Invert picture colors
     '''
-    inverted_array:np.ndarray = np.array(list(map(invert_row, array)))
-    inverted_image:Image = Image.fromarray(inverted_array)
-    inverted_image.show()
+    new_array:np.ndarray = np.array(list(map(invert_row, array)))
+    new_image:Image = Image.fromarray(new_array)
+    new_image.show()
+    return new_array
 
 def ft_red(array:np.ndarray) -> np.ndarray:
     '''
     Red picture colors
     '''
-    inverted_array:np.ndarray = np.array(list(map(red_row, array)))
-    inverted_image:Image = Image.fromarray(inverted_array)
-    inverted_image.show()
+    new_array:np.ndarray = np.array(list(map(red_row, array)))
+    new_image:Image = Image.fromarray(new_array)
+    new_image.show()
+    return new_array
 
 
 def ft_green(array:np.ndarray) -> np.ndarray:
     '''
     Green picture colors
     '''
-    inverted_array:np.ndarray = np.array(list(map(invert, array)))
-    inverted_image:Image = Image.fromarray(inverted_array)
-    inverted_image.show()
+    new_array:np.ndarray = np.array(list(map(green_row, array)))
+    new_image:Image = Image.fromarray(new_array)
+    new_image.show()
+    return new_array
 
 
 def ft_blue(array:np.ndarray) -> np.ndarray:
     '''
     Blue picture colors
     '''
-    inverted_array:np.ndarray = np.array(list(map(invert, array)))
-    inverted_image:Image = Image.fromarray(inverted_array)
-    inverted_image.show()
+    new_array:np.ndarray = np.array(list(map(blue_row, array)))
+    new_image:Image = Image.fromarray(new_array)
+    new_image.show()
+    return new_array
 
 
 def ft_grey(array:np.ndarray) -> np.ndarray:
     '''
     Gary picture colors
     '''
-    inverted_array:np.ndarray = np.array(list(map(invert, array)))
-    inverted_image:Image = Image.fromarray(inverted_array)
-    inverted_image.show()
+    new_array:np.ndarray = np.array(list(map(grey_row, array)))
+    new_image:Image = Image.fromarray(new_array)
+    new_image.show()
+    return new_array
 
